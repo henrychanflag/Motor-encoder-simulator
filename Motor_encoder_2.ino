dc663e2099571aacd21239f4f065059460fd12b7
henrychanflag <henry.yk.chan@polyu.edu.hk>
2021-06-01 23:05:42 +0800

    2nd release

--- Motor_encoder_2.ino
+++ Motor_encoder_2.ino
@@ -1,5 +1,5 @@
-//Motor with Encoder Simulator V1.0
-//By Henry Chan, 07Jan2021
+//Motor with Encoder Simulator V2.0
+//By Angus Wong, 13May2021
 //Use Pro-Mini 3.3V, 8MHz
 //Convert the DC input voltage to an encoder output (Phase A and Phase B)
 //Frequency is from 100Hz to 1000Hz, inversely proportional to DC input
@@ -7,6 +7,7 @@
 //Add artificial variation of +/-5% to the motor speed
 
 #define DCinput A0
+#define DCinput1 A1
 #define variation A2
 #define encoder_out_A 13
 #define encoder_out_B 12
@@ -14,6 +15,7 @@
 
 Neotimer mytimer(200); // Set timer interrupt to 500ms
 int adc = 0;
+int adc1 = 1;
 int adc2 = 512;
 long delay_time = 0;
 int fraction = 100;
@@ -38,12 +40,15 @@ void loop() {
   // Read DC voltage and update encoder frequency every 0.2s
   if (mytimer.repeat()){
     adc = analogRead(DCinput);
+    adc1 = analogRead(DCinput1);
+    //Serial.println(adc);
+    //Serial.println(adc1);
   }
-  if (adc>1000){ //Encoder idle if PWM duty cycle is too low
+  if (adc>1000 && adc1>1000){ //Encoder idle if PWM duty cycle is too low
     digitalWrite(encoder_out_A, HIGH);
     digitalWrite(encoder_out_B, HIGH);
   }
-  else {
+  else if (adc < 1000){
     delay_time = map(adc, 60,1000,300,5000);// map to 100Hz, 1000Hz
     delay_time = delay_time * fraction/100;
     //Serial.println(delay_time);
@@ -63,4 +68,26 @@ void loop() {
     digitalWrite(encoder_out_B, HIGH);
     delayMicroseconds(delay_time/2);//A=0, B=1
   }
-}
+  
+  else {
+    delay_time = map(adc1, 60,1000,300,5000);// map to 100Hz, 1000Hz
+    delay_time = delay_time * fraction/100;
+    //Serial.println(delay_time);
+    digitalWrite(encoder_out_B, LOW);
+    digitalWrite(encoder_out_A, LOW);
+    delayMicroseconds(delay_time/2);//A=0, B=0
+  
+    digitalWrite(encoder_out_B, HIGH);
+    digitalWrite(encoder_out_A, LOW);
+    delayMicroseconds(delay_time/2);//A=0, B=1
+  
+    digitalWrite(encoder_out_B, HIGH);
+    digitalWrite(encoder_out_A, HIGH);
+    delayMicroseconds(delay_time/2);//A=1, B=1
+  
+    digitalWrite(encoder_out_B, LOW);
+    digitalWrite(encoder_out_A, HIGH);
+    delayMicroseconds(delay_time/2);//A=1, B=0
+  }
+  
+} 
